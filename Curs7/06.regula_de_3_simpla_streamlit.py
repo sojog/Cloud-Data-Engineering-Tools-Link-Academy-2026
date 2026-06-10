66ce6c72b77be506a77850dd32481b19c666fec8
Silviu Ojog <sil.ojog@gmail.com>
2026-06-10 17:38:20 +0300

    Procent din Bitcoin

---
+++ Curs7/06.regula_de_3_simpla_streamlit.py
@@ -0,0 +1,34 @@
+## Pentru instalare
+##  pip install streamlit
+
+## Pentru rulare
+## streamlit run numele_fisierului.py
+import streamlit as st
+import requests
+
+
+st.title("Calculeaza procent din pretul Bitcoin")
+
+SYMBOLS = ["EUR", "USDT"]
+
+def get_prices(symbols:list[str]):
+    responses = []
+    for currency in symbols:
+        response = requests.get(f"https://api.binance.com/api/v3/avgPrice?symbol=BTC{currency}")
+        responses.append((currency, float(response.json()["price"])))
+    return responses
+
+responses = get_prices(SYMBOLS)
+for currency, price in  responses:
+    st.json({"price":price, "currency": currency})
+
+
+input_number = st.number_input("Insereaza textul tau si apoi apasă ENTER", min_value=0.01, max_value=100.0, value=0.07)
+if input_number:
+    text = f"{input_number} % dintr-un Bitcoin este: "
+    for currency, price in  responses:
+        valoare_procentuala = price * input_number 
+        text += f"{round(valoare_procentuala, 2)} {currency}  "
+    st.write(text)
+
+ 
