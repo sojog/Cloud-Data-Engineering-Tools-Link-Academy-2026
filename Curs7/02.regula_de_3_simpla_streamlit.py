7e5469e5e0a27b27ac0d27bbdb4a574a1618ead4
Silviu Ojog <sil.ojog@gmail.com>
2026-06-10 12:33:42 +0300

    Reformat

--- Curs7/02.regula_de_3_simpla_streamlit.py
+++ Curs7/02.regula_de_3_simpla_streamlit.py
@@ -13,10 +13,10 @@ response = requests.get("https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT"
 st.json(response.json())
 
 
-input_number = st.number_input("Insereaza textul tau", min_value=0.01, max_value=100.0, value=0.07)
+input_number = st.number_input("Insereaza textul tau si apoi apasă ENTER", min_value=0.01, max_value=100.0, value=0.07)
 if input_number:
     bitcoin = float(response.json()["price"])
-    valoare_procentuala = bitcoin * input_number / 100
-    st.write(f"{input_number} % dintr-un Bitcoin este:", valoare_procentuala)
+    valoare_procentuala = bitcoin * input_number 
+    st.write(f"{input_number} % dintr-un Bitcoin este:", round(valoare_procentuala, 2), "dolari")
 
  
